6cac906c997c04862fc2cf37f0691f21d7b8970b
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2021-02-14 16:56:46 +0300

    many selects

--- js/index.js
+++ js/index.js
@@ -1,21 +1,29 @@
-const select = document.querySelector('[data-type="select"]')
+function setupSelect(currentSelect) {
+    const select = currentSelect
+    if (select) {
+        const items = select.querySelectorAll('[data-type="item"]')
+        const array = [...items]
+        let obj = array.map((item) => {
+            return {
+                id: item.dataset.id,
+                value: item.textContent
+            }
+        })
 
-if (select) {
-    const items = select.querySelectorAll('[data-type="item"]')
-    const array = [...items]
-    let obj = array.map((item) => {
-        return {
-            id: item.dataset.id,
-            value: item.textContent
-        }
-    })
-
-    new Select('#select',  {
-        placeholder: 'Выберите язык программирования',
+        new Select(select,  {
+            placeholder: 'Выберите язык программирования',
             selectedId: '1',
             data: obj,
             onSelect(item) {
-            console.log('[index.js] Selected item:', item)
-        }
-    })
+                console.log('[index.js] Selected item:', item)
+            }
+        })
+    }
 }
+
+function callSelects() {
+    const selects = document.querySelectorAll('[data-type="select"]')
+    if (selects) selects.forEach(select => setupSelect(select))
+}
+
+callSelects()
--- js/select.js
+++ js/select.js
@@ -1,11 +1,11 @@
 class Select {
-    constructor(selector, options) {
-        this.$el = document.querySelector(selector)
+    constructor(select, options) {
+        this.$el = select
         this.options = options
         this.selectedId = options.selectedId
-        this.inputHidden = document.querySelector('[data-input="hidden"]')
+        this.inputHidden = this.$el.querySelector('[data-input="hidden"]')
         this.inputHiddenValue = null
-        this.span = document.querySelector('[data-type="value"]')
+        this.span = this.$el.querySelector('[data-type="value"]')
         this.render()
         this.setup()
     }
@@ -24,7 +24,6 @@ class Select {
         this.clickHandler = this.clickHandler.bind(this)
         if (this.$el) {
             this.$el.addEventListener('click', this.clickHandler)
-            this.$arrow = this.$el.querySelector('[data-type="arrow"]')
             this.$value = this.$el.querySelector('[data-type="value"]')
         }
     }
@@ -70,16 +69,16 @@ class Select {
 
     open() {
         this.$el.classList.add('open')
-        this.$arrow.classList.remove('fa-chevron-down')
-        this.$arrow.classList.add('fa-chevron-up')
-
-        const selectDropDown = document.querySelector('.select__dropdown')
+        const selectDropDown = this.$el.querySelector('.select__dropdown')
         selectDropDown.style.overflowY = 'auto'
     }
 
     close() {
         this.$el.classList.remove('open')
-        this.$arrow.classList.add('fa-chevron-down')
-        this.$arrow.classList.remove('fa-chevron-up')
+    }
+
+    destroy() {
+        this.$el.removeEventListener('click', this.clickHandler)
+        this.$el.innerHTML = ''
     }
 }
